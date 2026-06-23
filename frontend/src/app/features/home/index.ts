a01af6e729287466513cbf94949454fd755a709d
LuanVRD <luan.vrd20@gmail.com>
2026-06-23 11:05:43 -0300

    feat: initialize frontend project with Angular 21 structure and routing setup

---
+++ frontend/src/app/features/home/index.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './template.html',
+  styleUrl: './style.scss',
+})
+export class HomeComponent { }
