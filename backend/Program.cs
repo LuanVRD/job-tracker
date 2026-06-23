9d29f0cc1ac8977d121bb53089752854536da1d3
LuanVRD <luan.vrd20@gmail.com>
2026-06-23 09:47:20 -0300

    feat: initialize backend project with Entity Framework Core, PostgreSQL, and Scalar OpenAPI support

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,17 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
