24459d66a761b37849f717a644290189d604d3f9
LuanVRD <luan.vrd20@gmail.com>
2026-06-24 11:03:11 -0300

    feat: add DTOs for user authentication and registration requests and responses

---
+++ backend/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.DTOs.Auth
+{
+    public class AuthResponse
+    {
+        public required string Token { get; set; }
+        public required string UserName { get; set; }
+        public required string UserEmail { get; set; }
+    }
+}
