69d360b9da501f948ff6dbc6d89bb89015ae1da4
LuanVRD <luan.vrd20@gmail.com>
2026-06-29 12:02:25 -0300

    feat: implement authentication service and API interceptor for HTTP requests

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { apiInterceptor } from './core/interceptors/api-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([apiInterceptor])
+    ),
+    provideAnimations(),
+  ],
 };
---
+++ frontend/src/app/core/interceptors/api-interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const baseUrl = 'http://localhost:5169/api'
+  const token = localStorage.getItem('token');
+
+  let cloneReq = req.clone({
+    url: req.url.startsWith('/') ? `${baseUrl}${req.url}` : req.url,
+  });
+
+  if (token) {
+    cloneReq = cloneReq.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
+  return next(cloneReq);
+};
