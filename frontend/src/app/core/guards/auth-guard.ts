7d842c18c57e57a9e808eb7de9a68c644a562467
LuanVRD <luan.vrd20@gmail.com>
2026-07-01 11:17:13 -0300

    feat: add auth guard to restrict route access based on authentication status

--- frontend/src/app/core/guards/auth-guard.ts
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -1,11 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '@app/features/auth/services/auth';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const token = localStorage.getItem('token');
+  const authService = inject(AuthService);
 
-  if (token) {
+  if (authService.isAuthenticated()) {
     return true;
   }
 
